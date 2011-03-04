2e7b8aee078e05ed140902f8bbe2c0be0f1db6c5
Douglas de Almeida Costa <docosta2@abril.com.br>
2011-03-04 12:51:59 -0300

    Correcao do nome das rotas

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   map.car_index   "cars",     :controller => "cars", :action => "index",    :conditions => {:method => :get}
   map.car_show    "cars/:id", :controller => "cars", :action => "show",     :conditions => {:method => :get}
   map.car_create  "cars",     :controller => "cars", :action => "create",   :conditions => {:method => :post}
-  map.car_create  "cars/:id", :controller => "cars", :action => "update",   :conditions => {:method => :put}
-  map.car_create  "cars/:id", :controller => "cars", :action => "destroy",  :conditions => {:method => :delete}
+  map.car_update  "cars/:id", :controller => "cars", :action => "update",   :conditions => {:method => :put}
+  map.car_destroy "cars/:id", :controller => "cars", :action => "destroy",  :conditions => {:method => :delete}
 end
